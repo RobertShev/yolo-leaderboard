1835bdb82ce1212eda29a954e2332083413541c2
Robert Shevchenko <roberto.shev@gmail.com>
2023-09-26 13:12:47 +0300

    Add support for dark theme based on prefers-color-scheme

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,27 @@
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import {
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  useMediaQuery,
+} from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 
+import { useMemo } from 'react';
 import Routing from '../pages';
 
-const theme = createTheme({});
-
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline enableColorScheme />

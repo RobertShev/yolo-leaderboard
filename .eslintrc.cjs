c62a0b1536b110d9d9affe2432af6725e74357a3
Robert Shevchenko <roberto.shev@gmail.com>
2023-09-26 09:38:31 +0300

    Update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,47 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb",
+        "airbnb-typescript",
+        "airbnb/hooks",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended"
     ],
-  },
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "./tsconfig.json",
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react",
+        "prettier",
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": 0,
+        "react/jsx-props-no-spreading": 0,
+        "react/require-default-props": 0,
+        "react-hooks/exhaustive-deps": 0,
+        "import/no-extraneous-dependencies": 0,
+        "no-restricted-exports": 0,
+    },
+    "ignorePatterns": [".eslintrc.cjs", "vite.config.ts"],
 }
